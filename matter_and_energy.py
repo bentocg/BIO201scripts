93f2db0358f8ce715675dc93b9a13636eb4527ba
BentoCG <bentocollares@gmail.com>
2017-10-24 15:45:22 -0400

    first commit

--- matter_and_energy.py
+++ matter_and_energy.py
@@ -1,1 +1,12 @@
-print("hello world!")
+# import packages
+#TODO import answers from spreadsheet
+#TODO find matches
+#TODO do some descriptve stats
+import os
+import numpy as np
+import pandas as pd
+from difflib import SequenceMatcher
+from functools import reduce
+
+# change working directory
+os.chdir("C:/Users/bento/OneDrive/Documents/BIO201 scripts/format_midterm/FALL 17/midterm 1 section 2/")
